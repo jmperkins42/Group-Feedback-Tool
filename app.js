48fda19fbf9f19043da3eb6b3c65f3d10b83cae4
Gavin Walker <142553471+gavinwalker04@users.noreply.github.com>
2025-03-24 20:49:19 -0500

    JavaScript added for instructor registration

--- app.js
+++ app.js
@@ -271,3 +271,82 @@ document.querySelector('#btnRegister').addEventListener('click', function(){
         //createUser(strEmail,strPassword,strFirstName,strLastName)
     }
 })
+
+// click event for btnInstructorRegister
+document.querySelector('#btnInstructorRegister').addEventListener('click', function(){
+    // Define a function to create a user
+   
+
+    // Retrieve the values from your registration form
+    const strEmail = document.querySelector('#txtInstructorUsernameRegister').value.trim().toLowerCase()
+    const strPassword = document.querySelector('#txtInstructorPasswordRegister').value
+    const strConfirmPassword = document.querySelector('#txtInstructorRetypePassword').value
+    const strFirstName = document.querySelector('#txtInstructorFirstName').value
+    const strMiddleName = document.querySelector('#txtInstructorMiddleName').value
+    const strLastName = document.querySelector('#txtInstructorLastName').value
+    const strPhoneNumber = document.querySelector('#txtInstructorPhoneNumber').value
+    const strDiscordName = document.querySelector('#txtInstructorDiscordName').value
+    const strTeamsEmail = document.querySelector('#txtInstructorTeamsEmail').value
+    const strOrgType = document.querySelector('#cboInstructorOrgType').value
+    const strOrgName = document.querySelector('#txtInstructorOrgName').value
+
+    let blnError = false
+    let strMessage = ''
+
+    // Validate the data
+    if (!regEmail.test(strEmail)){
+        blnError = true
+        strMessage += '<p>Email is invalid</p>'
+    }
+
+    if (strPassword.length < 8){
+        blnError = true
+        strMessage += '<p>Password must be at least 8 characters</p>'
+    }
+
+    if (strPassword != strConfirmPassword){
+        blnError = true
+        strMessage += '<p>Passwords do not match</p>'
+    }
+
+    if (strFirstName.length < 2){
+        blnError = true
+        strMessage += '<p>First name must be at least 2 characters</p>'
+    }
+
+    if (strLastName.length < 2){
+        blnError = true
+        strMessage += '<p>Last name must be at least 2 characters</p>'
+    }
+
+    if (strPhoneNumber.length < 10 || strPhoneNumber.length > 15){
+        blnError = true
+        strMessage += '<p>Phone Number is invalid</p>'
+    }
+
+    if (strOrgType.length < 1){
+        blnError = true
+        strMessage += '<p>Must select an organization type</p>'
+    }
+
+    if (strOrgName.length < 2){
+        blnError = true
+        strMessage += '<p>Please input an organization name</p>'
+    }
+
+
+    if(blnError == true){
+        Swal.fire({
+            title:'Look a little closer, there may be errors',
+            html:strMessage,
+            icon:'error'
+        })
+    } else {
+        Swal.fire({
+            title:'Account Created!',
+            icon:'success'
+        })
+        // Call our function to create the account
+        //createUser(strEmail,strPassword,strFirstName,strLastName)
+    }
+})
